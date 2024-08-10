15ccf4634e23e1b16a6395937691c93ba3f815e9
beehelit <satourugojo@yandex.ru>
2024-08-10 16:39:02 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM maven:3.9.8-eclipse-temurin-22-alpine
+RUN apk update && apk add openssh
+
+# SSH
+EXPOSE 22
+RUN echo "StrictHostKeyChecking=no" >> /etc/ssh/ssh_config
+RUN mkdir /var/run/sshd
+RUN ssh-keygen -A
+CMD ["/usr/sbin/sshd", "-D"]
---
+++ src/main/java/su/beehelit/codetask/CodetaskApplication.java
@@ -0,0 +1,13 @@
+package su.beehelit.codetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetaskApplication.class, args);
+	}
+
+}
