e60930b255a1fbd45ee8b0405b3d272f7d7e7551
beehelit <satourugojo@yandex.ru>
2024-08-11 12:02:29 +0300

    add page login

---
+++ src/main/java/su/beehelit/codetask/pages/LoginController.java
@@ -0,0 +1,12 @@
+package su.beehelit.codetask.pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String home() {
+        return "login";
+    }
+}
